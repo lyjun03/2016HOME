e62c2fcff9d4423cbb2b0d334716a12b0e98af6c
lyjun03@naver.com <lyjun03@naver.com>
2016-08-26 18:34:48 +0900

    LineFriends

---
+++ linefriends/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./libs/css/webfont.css" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="./libs/css/common.css" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="./libs/css/main.css" title="no title" charset="utf-8">
+    <title>Document</title>
+
+</head>
+<body>
+    <div id="wrapper">
+        <div id="header">
+            <div class="headers">
+                <h1 class="logo">
+                    <a href="/linefriends">
+                        <span>LINE FRIENDS</span>
+                    </a>
+                </h1>
+                <div class="nav">
+                    <ul>
+                        <li>
+                            <a href="">BUSINESS</a>
+                        </li>
+                        <li>
+                            <a href="">STORE</a>
+                        </li>
+                        <li>
+                            <a href="">GALLERY</a>
+                        </li>
+                        <li>
+                            <a href="">ABOUT</a>
+                        </li>
+                        <li>
+                            <a href="http://store.linefriends.com" target="_blank">ONLINE STORE</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="select_lang">
+                    <div>
+                        <a href="" class="select_opt">한국어</a>
+                    </div>
+                    <ul>
+                        <li><a href="?lang=ko" >한국어</a></li>
+        				<li><a href="?lang=en" >ENGLISH</a></li>
+        				<li><a href="?lang=jp" >日本語</a></li>
+        				<li><a href="?lang=zh_CN" >中文(简体)</a></li>
+        				<li><a href="?lang=zh_TW" >中文(繁體)</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
